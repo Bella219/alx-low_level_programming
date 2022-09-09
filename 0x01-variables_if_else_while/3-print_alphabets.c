e01ae04fe4110fd9efa32b53c0648f356871e77e
Bella219 <cho219bella@yahoo.com>
2022-09-09 16:02:41 -0700

    Prints alphabeth in lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+	int CH;
+
+	for (ch = 'a'; ch <= 'z'; CH++)
+	{
+	putchar(CH);
+
+	}
+	for (CH = 'A'; CH <= 'Z'; CH++)
+	{
+	putchar('\n');
+	}
+
+	return (0);
+}
