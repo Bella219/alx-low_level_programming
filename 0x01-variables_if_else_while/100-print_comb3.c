d67d17591dcf6bbfee2a9e2cd4a881433ce56da0
Bella219 <cho219bella@yahoo.com>
2022-09-10 14:41:13 -0700

    Print all possible combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,19 +10,22 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 0; n <= 8; n++)
 	{
-	for (m = 48; m <= 57; m++)
-	{
-	putchar(n);
-	putchar(m);
+		for (m = 1; m <= 9; m++)
+		{
 
-	if (n != 57 || m != 57)
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
+			if (n != m && n < m)
+			{
+				putchar(n + '0');
+				putchar(m + '0');
+				if (!(n == 8 && m == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
