e51a02ae002ad3a1cad49dadb7919f84c716686d
Bella219 <cho219bella@yahoo.com>
2022-09-13 14:08:00 -0700

    Print 10 times alphabeth in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Print 10 times alphabeth in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	_putchar(a);
+
+	}
+	_putchar('\n');
+
+	}
+}
