d1e88203de948a8b6b1917e39b155ace57ab8d8d
Bella219 <cho219bella@yahoo.com>
2022-09-14 16:57:19 -0700

    print n times table starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,6 +3,7 @@
 /**
  * print_times_table - printing the n times table, starting woth 0
  * @n: number of the time table
+ * Return: returns nothing
  */
 
 void print_times_table(int n)
@@ -38,7 +39,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
-					_putchar((k / 10) % '0');
+					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 			}
