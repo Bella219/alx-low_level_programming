88c1321fbabe8833323ee73c4265cd590c8ddd03
Bella219 <cho219bella@yahoo.com>
2022-09-13 19:58:46 -0700

    print 9 times table beginning with o

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,37 +8,26 @@
 
 void times_table(void)
 {
-	int x, y, product;
+	int num, mult, prod;
 
-	for (x = 0; x <= 9; x++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (y = 0; y <= 9; y++)
+
+		_putchar('0');
+		for (mult = 1; mult <= 9; mult++)
 		{
-			product = x * y;
-			if (y != 0)
-			{
+			_putchar(',');
+			_putchar(' ');
 
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (y == 0)
-			{
+			prod = num * mult;
 
-				_putchar('0');
-			}
-			else if (product >= 10)
-			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-
-			}
-			else if ((product < 10) && (y != 0))
-			{
-				_putchar(' ');
-				_putchar((product % 10) + '0');
-			}
+			if (prod <= 9)
+				_putchar (' ');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
-		_putchar('\n');	
+		_putchar('\n');
+
 	}
 }
-
