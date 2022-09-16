679b5817e9c78868f7bb15e5656613fcb25e5b96
Bella219 <cho219bella@yahoo.com>
2022-09-15 17:06:41 -0700

    Multiplying two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * @c: tested character
+ * Return: 1 if it is, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+	{
+		return (i);
+	}
+
+	return (0);
+}
