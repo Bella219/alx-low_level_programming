97957bbb4d39bd9e73f19d028a754bd2b3381f55
Bella219 <cho219bella@yahoo.com>
2022-09-16 05:35:45 -0700

    Print a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 	{
 		for (inc1 = 1; inc1 <= size; inc1++)
 		{
-			for ((inc2 = size - inc1); inc2 > 0; inc2++)
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
 			{
 				putchar(' ');
 			}
@@ -27,3 +27,11 @@ void print_triangle(int size)
 			if (inc1 == size)
 			{
 				continue;
+			}
+			putchar('\n');
+
+		}
+
+	}
+	putchar('\n');
+}
