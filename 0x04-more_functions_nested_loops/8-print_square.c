3a1b39320b53be3ae889599734f66bedb41cf3df
Bella219 <cho219bella@yahoo.com>
2022-09-16 03:43:45 -0700

    Print squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * pprint_square - prints squares
+ * print_square - prints squares
  * @size: parameter
  * Return: returns nothing
  */
@@ -13,7 +13,7 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (inc1 = 0; inc1 < size, inc1++)
+		for (inc1 = 0; inc1 < size; inc1++)
 		{
 
 			for (inc2 = 0; inc2 < (size - 1); inc2++)
